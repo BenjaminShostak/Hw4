203cbfcf2fd3160b86ffb2bb75311f0978cb05c1
Benjamin Shostak <benshostak@rocketmail.com>
2020-10-11 23:21:47 -0700

    done!

--- Script.js
+++ Script.js
@@ -1,25 +1,56 @@
 const startButton = document.getElementById('start-btn')
+const nextButton = document.getElementById('next-btn')
 const questionContainerElement = document.getElementById
  ('question-container')
 
- const shuffledQuestions, currentQuestionIndex
+ const questionElement = document.getElementById('questions')
+ const answerButtonsElement = document.getElementById('answer-buttons')
+
+ let shuffledQuestions, currentQuestionIndex
 
 function startGame() {
 startButton.classList.add('hide')
-shuffledQuestions = questions.sort(()) => Math.random() - .5)
+shuffledQuestions = questions.sort(() => Math.random() - .5)
 currentQuestionIndex = 0
 questionContainerElement.classList.remove ('hide') 
 
-setNextQuestion(){
-    showQuestion(question)
+setNextQuestion(); {
+    showQuestion(question); {
+        questionElement.innerText = question.question
+        question.answers.array.forEach(answer => {
+            const button = document.createElement('button')
+            button.innerText = answer.text
+            button.classList.add('btn')
+            if (answer.correct) {
+                button.dataset.correct = answer.correct
+            }
+            button.addEventListener('click', selectAnswer)
+            answerButtonsElement.appendChild(button)
+        });
+
+    }
 }
+
+function resetState(){
+    nextbutton.classList.add('hide')
+    while (answerButtonsElement.firstChild) {
+        answerButtonsElement.removeChild
+        (answerButtonsElement.firstChild)
+    }
 
 function setNextQuestion() {
-
+    
+    showQuestion(shuffledQuestions[currentQuestionIndex])
 }
 
-function selectAnswer() {
+resetState() {
+    
+    }
+}
 
+function selectAnswer(e) {
+    const selectedButton = e.target
+    const correct = selectedButton.dataset.correct
 }
 
 console.log('Started')
@@ -29,12 +60,8 @@ const questions = [
     { 
     question: 'Who is Samuel Clemens ?',
     answers: [
-        { text: 'Author: Mark Twaine', correct: true}
-        { text: 'Crime Boss: Alphonse Capone', correct: false }
-        { text: 'Actor: Morgan Freedman', correct: false }
+        { text: 'Author: Mark Twaine', correct: true},
+        { text: 'Crime Boss: Alphonse Capone', correct: false },
+        { text: 'Actor: Morgan Freedman', correct: false },
         { text: 'Author: Steven King', correct: false }
-           
-        }
     ]
-    }
-]
